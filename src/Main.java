89eb3b5eead1a86a0f0fe50f4608efde42100aeb
Konstantin <reverant_01@mail.ru>
2024-01-29 21:14:32 +0300

    ДЗ №10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        //task2
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчёта - " + fullName.toUpperCase());
+
+        //task3
+        fullName = "Иванов Иван Семёнович";
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace("ё", "е"));
+
+
     }
 }
